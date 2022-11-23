78fb81bf6915cf5528e718cb901358c2f9c8e29f
Lukas Cerny <pw21l013@technikum-wien.at>
2022-11-23 12:32:04 +0100

    init commit

---
+++ app/src/main/java/rechteckflaeche/exceptions/LengthNegativeException.java
@@ -0,0 +1,7 @@
+package rechteckflaeche.exceptions;
+
+public class LengthNegativeException extends Exception {
+    public LengthNegativeException(String msg) {
+        super(msg);
+    }
+}
---
+++ app/src/main/java/rechteckflaeche/exceptions/LengthTooLongException.java
@@ -0,0 +1,7 @@
+package rechteckflaeche.exceptions;
+
+public class LengthTooLongException extends Exception {
+    public LengthTooLongException(String msg) {
+        super(msg);
+    }
+}
